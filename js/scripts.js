df6376a0a9eba51b387773e05f3efea828b9f0ce
Nadiya Yeroshkina <nadin.yeroshkina@email.com>
2016-04-08 19:52:22 -0700

    add logic and calculation for pizza size

--- js/scripts.js
+++ js/scripts.js
@@ -3,17 +3,12 @@
 $(document).ready(function() {
   $("#submit-order").submit(function(event) {
     event.preventDefault();
-    var pizzaSize = $(".size").val();
-    console.log();
-    var topping = $().val();
-    var sizeInput = $("#size").val();
-
+    var pizzaSize = $("#size-list").val();
+    var toppings;
+    var newPizza = new Pizza(pizzaSize, toppings);
+    var result = newPizza.costCalc();
   });
 });
-
-
-
-
 
 //business logic
 
@@ -24,17 +19,34 @@ function Pizza(size, toppings) {
 }
 //prototype method
 Pizza.prototype.costCalc = function(){
-  return this.inputSize.val() + '' + this.inputToppings.val();
+  // return this.inputSize.val() + '' + this.inputToppings.val();
   ///fix it, so it can calculate the cost!///// ;
+  var total;
+  if (this.inputSize === "small"){
+    return total = 8;
+  } else if (this.inputSize === "medium"){
+    return total = 11;
+  } else {
+    return total = 16;
+  }
+
+  // for (i =0; i <= this.toppings.length; i++){
+  //   if (this.toppings.length <= 1){
+  //     total;
+  //   } else {
+  //     total += this.toppings.length *1;
+  //   }
+  // }
+  return total;
 }
 
-var pizzaOrder = function(){
-  var orderedItems = [];
-  var userInput =
-  var size = ["Small", "Medium", "Large"];
-  var toppings = ["Ham", "Salami", "Pepperoni", "Bell pepers", "Tomatos", "Brovoli"]
-  toppings.forEach(function(topping){
-    if (var i=0; i = )
-  })
-  return orderedItems;
-};
+// var pizzaOrder = function(){
+//   var orderedItems = [];
+//   var userInput =
+//   var size = ["Small", "Medium", "Large"];
+//   var toppings = ["Ham", "Salami", "Pepperoni", "Bell pepers", "Tomatos", "Brovoli"]
+//   toppings.forEach(function(topping){
+//     if (var i=0; i = )
+//   })
+//   return orderedItems;
+// };
